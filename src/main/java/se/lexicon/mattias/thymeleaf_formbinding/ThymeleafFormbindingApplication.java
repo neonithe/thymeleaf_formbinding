651e77779c762f1f4c1d499863d5834ac6e47d22
Mattias Andersson <neonithe@gmail.com>
2020-09-03 13:32:40 +0200

    Initial commit Added packages Setup mysql

---
+++ src/main/java/se/lexicon/mattias/thymeleaf_formbinding/ThymeleafFormbindingApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.mattias.thymeleaf_formbinding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafFormbindingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafFormbindingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/mattias/thymeleaf_formbinding/ThymeleafFormbindingApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.mattias.thymeleaf_formbinding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafFormbindingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
